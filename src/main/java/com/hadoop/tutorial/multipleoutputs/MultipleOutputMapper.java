d0167fa3c536934e52edc58339fa532975348f30
baoyu.zhang <baoyu.zhang@corp.elong.com>
2016-11-08 09:48:59 +0800

    添加了multipleoutputs示例

---
+++ src/main/java/com/hadoop/tutorial/multipleoutputs/MultipleOutputMapper.java
@@ -0,0 +1,47 @@
+package com.hadoop.tutorial.multipleoutputs;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import java.io.IOException;
+
+/**
+ * Created by baoyu on 16/11/7.
+ */
+public class MultipleOutputMapper extends Mapper<Object,Text,Text,NullWritable> {
+
+    private MultipleOutputs<Text,NullWritable> mo = null;
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        mo = new MultipleOutputs<>(context);
+    }
+
+    @Override
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        if(value != null && value.toString().trim().length() > 0){
+            if(value.toString().contains("hadoop")) {
+                    mo.write("multiple",value,NullWritable.get(),"hadoop");
+            }
+
+            if(value.toString().contains("pig")) {
+                mo.write("multiple",value,NullWritable.get(),"pig");
+            }
+
+            if(value.toString().contains("hive")) {
+                mo.write("multiple",value,NullWritable.get(),"hive");
+            }
+
+            if(value.toString().contains("hbase")) {
+                mo.write("multiple",value,NullWritable.get(),"hbase");
+            }
+
+        }
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        mo.close();
+    }
+}
---
+++ src/main/java/com/hadoop/tutorial/multipleoutputs/MultipleoutputsMain.java
@@ -0,0 +1,43 @@
+package com.hadoop.tutorial.multipleoutputs;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+/**
+ * newsId join
+ */
+public class MultipleoutputsMain {
+          public static void main(String[] args) throws Exception{
+            if (args.length != 2) {
+                System.err.println("Usage: MultipleoutputsMain <input  path> <output path>");
+                System.exit(-1);
+            }
+            Job job = new Job();
+            job.setJarByClass(MultipleoutputsMain.class);
+            job.setJobName("MultipleoutputsMain");
+
+              FileInputFormat.addInputPath(job, new Path(args[0]));
+              FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+            if(FileSystem.get(job.getConfiguration()).exists(new Path(args[1]))){
+                FileSystem.get(job.getConfiguration()).delete(new Path(args[1]),true);
+            }
+              job.setMapperClass(MultipleOutputMapper.class);
+
+
+            MultipleOutputs.addNamedOutput(job,"multiple",TextOutputFormat.class,Text.class,NullWritable.class);
+            MultipleOutputs.setCountersEnabled(job,true);
+            job.setNumReduceTasks(0);
+              job.setOutputKeyClass(Text.class);              //注1
+            job.setOutputValueClass(NullWritable.class);
+
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+        }
+}
